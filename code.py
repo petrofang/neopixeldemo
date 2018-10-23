8308523b96c62649bc74b75fd166b734f6759dae
Petrofang <25374818+petrofang@users.noreply.github.com>
2018-10-22 23:36:36 -0400

    adjusted the increment of the brightness factor and made it more modular

--- code.py
+++ code.py
@@ -5,15 +5,16 @@ import neopixel
 import touchio
 
 # things you might want to adjust:
-nap = 0.0667            # time between frames (seconds)
-snooze = 1              # time to pause when complete
-COLOR1 = (248, 48, 0)   # (orange)
-COLOR2 = (0, 48, 0)     # (green)
-COLOR3 = (24, 0, 32)    # (purple)
+nap = 0.0667                    # time between frames (seconds)
+snooze = 1                      # time to pause when complete
+brightnessIncrement = 0.0667    # how much each touch 
+COLOR1 = (248, 48, 0)           # (orange)
+COLOR2 = (0, 48, 0)             # (green)
+COLOR3 = (24, 0, 32)            # (purple)
 
 # Create a neopixel array object for Hallowing with 30 neopixels.
 pin = board.EXTERNAL_NEOPIXEL
-STRIP = neopixel.NeoPixel(pin, 30, brightness=0.15, auto_write=True)
+STRIP = neopixel.NeoPixel(pin, 30, brightness=(brightnessIncrement*2), auto_write=True)
 
 # create touchio objects:
 touchA5 = touchio.TouchIn(board.A5)
@@ -22,12 +23,13 @@ touchA2 = touchio.TouchIn(board.A2)
 # check for touch input
 def checkForTouch():
     if touchA2.value:
-        if STRIP.brightness < 1:
-            STRIP.brightness += 0.05
+        if STRIP.brightness < (1 - brightnessIncrement):
+            STRIP.brightness += brightnessIncrement
+            print(STRIP.brightness)
     elif touchA5.value:
-        if STRIP.brightness > 0.05:
-            STRIP.brightness += -0.05
-
+        if STRIP.brightness > brightnessIncrement:
+            STRIP.brightness += -(brightnessIncrement)
+            print(STRIP.brightness)
 # rotate through alternating colors 
 while True:
     for x in range(0, 30, 2):
